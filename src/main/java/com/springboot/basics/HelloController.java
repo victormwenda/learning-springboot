3a1ea5d1c9518f42108103ff7f8cbdac078b40cd
Victor Mwenda <vmwenda.vm@gmail.com>
2018-12-22 09:40:13 +0300

    Creating unit tests

--- src/main/java/com/springboot/basics/HelloController.java
+++ src/main/java/com/springboot/basics/HelloController.java
@@ -3,11 +3,19 @@ package com.springboot.basics;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "Welcome to the world";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Welcome to the helloed world";
     }
 }
